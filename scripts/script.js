3d962f0751b3a56d305a5ee26bc5c6d3931f2754
roe-el <rogar28@gmail.com>
2018-01-06 08:23:55 -0600

    Add equal vars, newCalc/addOn functions, try/catch for eval, and keys/op listener.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,12 @@
+/***********************************************************************
+ ** Name: Roel Garcia
+ ** Date: 1/
+/ ** Description: File that contains the code for a simple web based
+ ** calc
+ ***********************************************************************/
 $(document).ready(function() {
     var math = [];
-    var a = [];
-    var b = [];
+    var equal, equal2 = false;
     $('.btn').hover(function() {
         $(this).addClass("focus");
     }, function() {
@@ -11,72 +16,63 @@ $(document).ready(function() {
     $('.ac-button').on('click', function() {
         $('.main-input').text('');
         $('.secondary-input').text('');
-    })
+    });
     $('.ce-button').on('click', function() {
         $('.main-input').text('');
-    })
-    $('.div-button').on('click', function() {
-        $('.main-input').append('/');
-        $('.secondary-input').append($('.main-input').text());
-        $('.main-input').text('');
-    })
-    $('.multiply-button').on('click', function() {
-        $('.main-input').append('*');
-        $('.secondary-input').append($('.main-input').text());
-        $('.main-input').text('');
-    })
-    $('.subtract-button').on('click', function() {
-        $('.main-input').append('-');
-        $('.secondary-input').append($('.main-input').text());
-        $('.main-input').text('');
-    })
-    $('.add-button').on('click', function() {
-        $('.main-input').append('+');
-        $('.secondary-input').append($('.main-input').text());
-        $('.main-input').text('');
-    })
-    $('.neg-button').on('click', function() {
-        $('.main-input').append('(-)');
-    })
+    });
+    $('.op-btn').on('click', function() {
+        addOn();
+        if ($(this).text() === "x") {
+            $('.main-input').append("*");
+            $('.secondary-input').append($('.main-input').text());
+            $('.main-input').text('');
+        } else {
+            $('.main-input').append($(this).text());
+            $('.secondary-input').append($('.main-input').text());
+            $('.main-input').text('');
+        }
+    });
     $('.dec-button').on('click', function() {
-        $('.main-input').append('.');
-    })
+        newCalc();
+        if ($('.main-input').text() === '') {
+            $('.main-input').append('0.');
+        } else {
+            $('.main-input').append('.');
+        }
+    });
     $('.eql-button').on('click', function() {
         $('.secondary-input').append($('.main-input').text());
-        
-        var math ='';
-        math = eval($('.secondary-input').text());
-        $('.main-input').text(math);
-        })
-        //Numbers
-    $('.9-button').on('click', function() {
-        $('.main-input').append('9');
-    })
-    $('.8-button').on('click', function() {
-        $('.main-input').append('8');
-    })
-    $('.7-button').on('click', function() {
-        $('.main-input').append('7');
-    })
-    $('.6-button').on('click', function() {
-        $('.main-input').append('6');
-    })
-    $('.5-button').on('click', function() {
-        $('.main-input').append('5');
-    })
-    $('.4-button').on('click', function() {
-        $('.main-input').append('4');
-    })
-    $('.3-button').on('click', function() {
-        $('.main-input').append('3');
-    })
-    $('.2-button').on('click', function() {
-        $('.main-input').append('2');
-    })
-    $('.1-button').on('click', function() {
-        $('.main-input').append('1');
-    })
-    $('.0-button').on('click', function() {
-        $('.main-input').append('0');
-    })
+
+        math = '';
+        try {
+            math = eval($('.secondary-input').text());
+            console.log("Math: " + math);
+            $('.main-input').text(math);
+            equal = equal2 = true;
+        } catch (err) {
+            $('.main-input').text("Error");
+            equal2 = true;
+            console.log(err);
+        }
+    });
+
+    function addOn() {
+        if (equal === true) {
+            $('.secondary-input').text('');
+            equal = equal2 = false;
+        }
+    }
+
+    function newCalc() {
+        if (equal2 === true) {
+            $('.main-input').text('');
+            $('.secondary-input').text('');
+            equal2 = false;
+        }
+    }
+    //Numbers
+    $('.key').on('click', function() {
+        newCalc();
+        $('.main-input').append($(this).text());
+    });
 });
